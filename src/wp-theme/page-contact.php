3d497917185db6b21bfabff6685ecb8e60e62ca0
Bence Agocs <bence@harom.studio>
2021-03-14 23:14:11 +0100

    contact page added

--- src/wp-theme/page-contact.php
+++ src/wp-theme/page-contact.php
@@ -11,39 +11,48 @@ get_header();
 
 <section class="section container">
 
-    <h1 class="mt-5">Kapcsolat</h1>
-    <div class="row mt-4">
+    <h1 class="mt-5"><?php the_title() ?></h1>
+    <div class="row bg-white p-3 mt-4">
         
         <div class="col-12 col-lg-4">
             <img src="" alt="">
         </div>
         <div class="col-12 col-lg-4">
         <table class="table table-hover">
-            <thead>
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-                </tr>
-            </thead>
             <tbody>
                 <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                    <th scope="row">Hétfő</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
                 </tr>
                 <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                    <th scope="row">Kedd</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
                 </tr>
                 <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
+                    <th scope="row">Szerda</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Csütörtök</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Péntek</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Szombat</th>
+                    <td>7:00</td>
+                    <td>12:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Vasárnapő</th>
+                    <td colspan="2">ZÁRVA</td>
                 </tr>
             </tbody>
             </table>
