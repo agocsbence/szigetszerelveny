dfcd98ae7d8d5f73970ca1c86dab904713459e12
Bence Agocs <bence@harom.studio>
2021-03-14 23:26:29 +0100

    contact page added

--- src/wp-theme/page-contact.php
+++ src/wp-theme/page-contact.php
@@ -69,6 +69,63 @@ get_header();
         </div>
 
     </div>
+
+    <div class="row bg-white p-3 mt-4">
+        
+        <div class="col-12 col-lg-4">
+            <h3>Taksony</h3>
+            <img src="https://placehold.it/640x360" alt="">
+            <p class="mt-3"><i class="bi bi-geo-alt"></i> 2335 Taksony Fő út 47.</p>
+            <p><a href="tel:+3624487029"><i class="bi bi-telephone"></i> +36 24 487 029</a></p>
+            <p><a href="mailto:info@szigetszerelveny.hu"><i class="bi bi-envelope"></i> info@szigetszerelveny.hu</a></p>
+        </div>
+        <div class="col-12 col-lg-4">
+        <table class="table table-hover">
+            <h3>Nyitvatartás</h3>
+            <tbody>
+                <tr>
+                    <th scope="row">Hétfő</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Kedd</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Szerda</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Csütörtök</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Péntek</th>
+                    <td>7:00</td>
+                    <td>17:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Szombat</th>
+                    <td>7:00</td>
+                    <td>12:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Vasárnapő</th>
+                    <td colspan="2">ZÁRVA</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        <div class="col-12 col-lg-4">
+            <h3>Térkép</h3>
+            google maps
+        </div>
+
+    </div>
 </section>
 
 <?php get_footer(); ?>
