e5f9431879847a9afeb22dfc46d900cafa64b5be
Bence Agocs <bence@harom.studio>
2021-03-14 23:09:57 +0100

    contact page added

---
+++ src/wp-theme/page-contact.php
@@ -0,0 +1,58 @@
+<?php
+/*
+
+Template Name: Kapcsolat
+
+*/
+
+get_header();
+
+?>
+
+<section class="section container">
+
+    <h1 class="mt-5">Kapcsolat</h1>
+    <div class="row mt-4">
+        
+        <div class="col-12 col-lg-4">
+            <img src="" alt="">
+        </div>
+        <div class="col-12 col-lg-4">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        <div class="col-12 col-lg-4">
+            google maps
+        </div>
+
+    </div>
+</section>
+
+<?php get_footer(); ?>
