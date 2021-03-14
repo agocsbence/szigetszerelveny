fbe8b37f54cec24a7421322463805642182d3ceb
Bence Agocs <bence@harom.studio>
2021-03-14 17:40:44 +0100

    archive updated

--- src/wp-theme/archive.php
+++ src/wp-theme/archive.php
@@ -1,31 +1,163 @@
-<?php get_header(); the_post(); ?>
+<?php get_header();
 
-	<div class="container">
-		<div class="">
+$id = get_the_ID();
 
-			<?php if ( have_posts() ) : ?>
+var_dump($id);
 
-			<div class="article-header">
-				<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+$parent_term_id = $id; // term id of parent term (edited missing semi colon)
+
+$taxonomies = array( 
+    'category',
+);
+
+$args = array(
+    // 'parent'         => $parent_term_id,
+    'child_of'      => $parent_term_id, 
+); 
+
+$terms = get_terms($taxonomies, $args);
+?>
+
+
+<section class="section products container">
+	<nav class="mt-5" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="termekek.html">Termékek</a></li>
+			<li class="breadcrumb-item active" aria-current="page">Víz-gáz-fűtés</li>
+		</ol>
+	</nav>
+	<div class="row mt-2">
+		<div class="col-sm-12 col-lg-3 g-3">
+			<div class="card">
+				<a href="">
+					<img src="./assets/img/csovek.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Csövek, csőrendszerek, idomok</h5>
+					</div>
+				</a>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item"><a href="single.html">Acélcső,idomok</a></li>
+					<li class="list-group-item">KPE cső, idomok</li>
+					<li class="list-group-item">Lefolyócső, idomok</li>
+					<li class="list-group-item">Ötrétegű cső, fitting</li>
+					<li class="list-group-item">Rézcső, idomok</li>
+					<li class="list-group-item">Szénacél cső, idomok</li>
+					<li class="list-group-item">Szigetelés, védőcső</li>
+				</ul>
 			</div>
-
-			<?php while ( have_posts() ) : the_post(); ?>
-				
-				<div class="article-content">
-					<?php the_content();?> 
-				</div>
-				
-			<?php endwhile;
-
-			else :
-			
-				echo 'No posts found';
-
-			endif;
-			?>
-        
+		</div>
+		<div class="col-sm-12 col-lg-3 g-3">
+			<div class="card">
+				<a href="">
+					<img src="./assets/img/mosogato.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Mosogatótálca, falikutak</h5>
+					</div>
+				</a>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+					<li class="list-group-item">A third item</li>
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+					<li class="list-group-item">A third item</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-12 col-lg-3 g-3">
+			<div class="card">
+				<a href="">
+					<img src="./assets/img/ontozes.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Öntözéstechnika</h5>
+					</div>
+				</a>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+					<li class="list-group-item">A third item</li>
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-12 col-lg-3 g-3">
+			<div class="card">
+				<a href="">
+					<img src="./assets/img/radiator.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Radiátor</h5>
+					</div>
+				</a>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+					<li class="list-group-item">A third item</li>
+					<li class="list-group-item">An item</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-12 col-lg-3 g-3">
+			<div class="card">
+				<a href="">
+					<img src="https://placehold.it/640x360" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Szerelvények</h5>
+					</div>
+				</a>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">An item</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-12 col-lg-3 g-3">
+			<div class="card">
+				<a href="">
+					<img src="https://placehold.it/640x360" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Szifonok</h5>
+					</div>
+				</a>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-12 col-lg-3 g-3">
+			<div class="card">
+				<a href="">
+					<img src="https://placehold.it/640x360" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Szivattyúk</h5>
+					</div>
+				</a>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+					<li class="list-group-item">A third item</li>
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-sm-12 col-lg-3 g-3">
+			<div class="card">
+				<a href="">
+					<img src="https://placehold.it/640x360" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Tágulási tartályok</h5>
+					</div>
+				</a>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">An item</li>
+					<li class="list-group-item">A second item</li>
+					<li class="list-group-item">A third item</li>
+					<li class="list-group-item">An item</li>
+				</ul>
+			</div>
 		</div>
 	</div>
-</main>
+</section>
 
 <?php get_footer();
