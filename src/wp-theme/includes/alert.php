f081ea3f233ef61dd17897f744fe55877c547395
Bence Agocs <bence@harom.studio>
2021-03-14 19:17:50 +0100

    archive updated

---
+++ src/wp-theme/includes/alert.php
@@ -0,0 +1,3 @@
+<section class="container mt-3">
+    <p class="alert alert-info" role="alert">Az oldalon nem a teljes kínálatot találja, látogasson el üzletünkbe vagy érdeklődjön elérhetőségeink valamelyikén raktárkészletünkről.</p>
+</section>
--- src/wp-theme/single.php
+++ src/wp-theme/single.php
@@ -3,16 +3,38 @@
 <?php the_title(); ?>
 <?php the_content(); ?>
 
-<h1>single.php</h1>
+<section class="section single container">
+    <nav class="mt-5" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="termekek.html">Termékek</a></li>
+            <li class="breadcrumb-item"><a href="<?php echo esc_url( get_category_link( $parent_category->term_id )) ?>"><?php echo $parent_category->name ?></a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?php echo $start_category->name ?></li>
+        </ol>
+      </nav>
+    <div class="row mt-4">
+        <div class="col-12 col-lg-4">
+            <img src="<?php echo the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>">
+        </div>
+        <div class="col-12 col-lg-8">
+            <h1><?php the_title(); ?></h1>
+            <p><?php the_content(); ?></p>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <h3>Kapcsolódó termékek</h3>
+        <?php
+            $loop = new WP_Query( array( 'post_type' => 'post', 'cat' => $start_category->term_id ) );
+            
+            while ( $loop->have_posts() ) : $loop->the_post(); 
+            
+                include get_theme_file_path( '/includes/card.php' );
+            
+            endwhile; wp_reset_query();
+        ?>
+
+    </div>
+</section>
 
 <?php
-    $loop = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => 3 ) );
-    
-    while ( $loop->have_posts() ) : $loop->the_post(); 
-    
-        include get_theme_file_path( '/includes/card.php' );
-    
-    endwhile; wp_reset_query();
-?>
-
-<?php get_footer();
+include get_theme_file_path( '/includes/alert.php' );
+get_footer();
