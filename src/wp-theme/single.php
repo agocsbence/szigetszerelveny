1acf59f84be6d2a73007a8d85cdb2fe2e07fcaf7
Bence Agocs <bence@harom.studio>
2021-03-14 22:47:02 +0100

    single.php updated

--- src/wp-theme/single.php
+++ src/wp-theme/single.php
@@ -7,7 +7,7 @@ $start_category = get_the_category($id);
 $parent_category_id = $start_category[0]->parent;
 $parent_category = get_term($parent_category_id, 'category');
 
-$master_category_id = $parent_category[0]->parent;
+$master_category_id = $parent_category->parent;
 $master_category = get_term($master_category_id, 'category');
 
 ?>
