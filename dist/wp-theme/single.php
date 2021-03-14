d38bb81ea643da2cfdbfc211ee91da868500cb60
Bence Agocs <bence@harom.studio>
2021-03-14 21:51:28 +0100

    page.php updated

--- dist/wp-theme/single.php
+++ dist/wp-theme/single.php
@@ -1,10 +1,19 @@
-<?php get_header(); ?>
+<?php get_header();
+
+$category = get_category( get_query_var( 'cat' ) );
+$cat_id = $category->cat_ID;
+
+$start_category = get_category($cat_id);
+$parent_category = get_category($start_category->parent);
+$master_category = get_category($parent_category->parent);
+?>
 
 <section class="section single container">
     <nav class="mt-5" aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="termekek.html">Termékek</a></li>
-            <li class="breadcrumb-item"><a href="<?php echo esc_url( get_category_link( $parent_category->term_id )) ?>"><?php echo $parent_category->name ?></a></li>
+            <li class="breadcrumb-item"><a href="<?php echo get_permalink(20); ?>">Termékek</a></li>
+            <li class="breadcrumb-item"><a href="<?php echo esc_url( get_category_link( $master_category->term_id )) ?>"><?php echo $master_category->name ?></a></li>
+            <li class="breadcrumb-item"><?php echo $parent_category->name ?></li>
             <li class="breadcrumb-item active" aria-current="page"><?php echo $start_category->name ?></li>
         </ol>
       </nav>
@@ -20,7 +29,7 @@
     <div class="row mt-4">
         <h3>Kapcsolódó termékek</h3>
         <?php
-            $loop = new WP_Query( array( 'post_type' => 'post', 'cat' => $start_category->term_id ) );
+            $loop = new WP_Query( array( 'post_type' => 'post', 'cat' => $start_category ) );
             
             while ( $loop->have_posts() ) : $loop->the_post(); 
             
