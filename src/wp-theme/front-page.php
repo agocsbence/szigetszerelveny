09e2462f88bc522601b9f5c7b0c8891189f4300f
Bence Agocs <bence@harom.studio>
2021-03-14 13:10:36 +0100

    ront page image url updated

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -17,22 +17,22 @@ get_header();
 			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
 		</div>
 		<div class="carousel-inner">
-			<div class="carousel-item active" data-bs-interval="5000" style="background-image: url('./assets/img/slider-1.jpg');">
-				<img src="./assets/img/fade.png" class="d-block w-100" alt="...">
+			<div class="carousel-item active" data-bs-interval="5000" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/slider-1.jpg');">
+				<img src="<?php bloginfo('template_url') ?>/assets/img/fade.png" class="d-block w-100" alt="...">
 				<div class="carousel-caption d-none d-md-block">
 					<h1>Víz-gáz-fűtésszerelési anyagok</h1>
 					<p>Some representative placeholder content for the first slide.</p>
 				</div>
 			</div>
-			<div class="carousel-item" data-bs-interval="5000" style="background-image: url('./assets/img/slider-2.jpg');">
-				<img src="./assets/img/fade.png" class="d-block w-100" alt="...">
+			<div class="carousel-item" data-bs-interval="5000" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/slider-2.jpg');">
+				<img src="<?php bloginfo('template_url') ?>/assets/img/fade.png" class="d-block w-100" alt="...">
 				<div class="carousel-caption d-none d-md-block">
 					<h1>Kondenzációs kazánok</h1>
 					<p>Some representative placeholder content for the first slide.</p>
 				</div>
 			</div>
-			<div class="carousel-item" data-bs-interval="5000" style="background-image: url('./assets/img/slider-3.jpg');">
-				<img src="./assets/img/fade.png" class="d-block w-100" alt="...">
+			<div class="carousel-item" data-bs-interval="5000" style="background-image: url('<?php bloginfo('template_url') ?>/assets/img/slider-3.jpg');">
+				<img src="<?php bloginfo('template_url') ?>/assets/img/fade.png" class="d-block w-100" alt="...">
 				<div class="carousel-caption d-none d-md-block">
 					<h1>Szaniter/fürdőszoba</h1>
 					<p>Some representative placeholder content for the first slide.</p>
