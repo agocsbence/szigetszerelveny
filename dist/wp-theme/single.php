a740fff355b40f998768dc47d4464d3eb51d0c54
Bence Agocs <bence@harom.studio>
2021-03-14 22:28:42 +0100

    single.php updated

--- dist/wp-theme/single.php
+++ dist/wp-theme/single.php
@@ -10,15 +10,15 @@ $parent_category = get_the_category($parent_category_id);
 $master_category_id = $parent_category[0]->parent;
 $master_category = get_the_category($master_category_id);
 
-var_dump($id);
+var_dump($parent_category);
 ?>
 
 <section class="section single container">
     <nav class="mt-5" aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="<?php echo get_permalink(20); ?>">Termékek</a></li>
-            <li class="breadcrumb-item"><a href="<?php echo esc_url( get_category_link( $master_category->term_id )) ?>"><?php echo $master_category->name ?></a></li>
-            <li class="breadcrumb-item"><?php echo $parent_category->name ?></li>
+            <li class="breadcrumb-item"><a href="<?php echo esc_url( get_category_link( $master_category[0]->term_id )) ?>"><?php echo $master_category[0]->name ?></a></li>
+            <li class="breadcrumb-item"><?php echo $parent_category[0]->name ?></li>
             <li class="breadcrumb-item active" aria-current="page"><a href="<?php echo esc_url( get_category_link( $start_category[0]->term_id )) ?>"><?php echo $start_category[0]->name ?></a></li>
         </ol>
       </nav>
