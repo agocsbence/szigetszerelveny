e3cc6653588675a5b23955900a922005ff1202a1
Bence Agocs <bence@harom.studio>
2021-03-14 19:04:55 +0100

    archive updated

---
+++ dist/wp-theme/includes/sub-category.php
@@ -0,0 +1,30 @@
+<section class="section single container">
+    <nav class="mt-5" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="termekek.html">Termékek</a></li>
+          <li class="breadcrumb-item"><a href="vizgaz.html">Víz-gáz-fűtés</a></li>
+          <li class="breadcrumb-item active" aria-current="page"><?php echo $start_category->name ?></li>
+        </ol>
+      </nav>
+    <div class="row mt-4">
+        <?php var_dump($start_category); ?>
+        <h1><?php echo $start_category->name ?></h1>
+        <p><?php echo $start_category->description ?></p>
+    </div>
+    <div class="row mt-4">
+
+        <?php
+            $loop = new WP_Query( array( 'post_type' => 'post', 'cat' => $start_category->term_id ) );
+            
+            while ( $loop->have_posts() ) : $loop->the_post(); 
+            
+                include get_theme_file_path( '/includes/card.php' );
+            
+            endwhile; wp_reset_query();
+        ?>
+
+    </div>
+</section>
+<section class="container mt-3">
+    <p class="alert alert-info" role="alert">Az oldalon nem a teljes kínálatot találja, látogasson el üzletünkbe vagy érdeklődjön elérhetőségeink valamelyikén raktárkészletünkről.</p>
+</section>
--- src/wp-theme/includes/card.php
+++ src/wp-theme/includes/card.php
@@ -1,20 +1,9 @@
-<div class="card">
-    <img src="<?php echo the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="card-image">
-    <div class="card-text">
-        <h3 class="title card-title"><?php the_title(); ?></h3>
-        <p class="card-description"><?php the_excerpt(); ?></p>
-    </div>
-    <div class="card-buttons">
-        <div class="btn btn-blue">
-            <div class="btn-text img-right">
-                <?php the_field('olvasasi_ido'); ?> Perc
-                <img src="<?php bloginfo('template_url') ?>/assets/img/clock.svg" alt="">
-            </div>
-        </div>
-        <a href="<?php echo get_permalink(); ?>" class="btn btn-plain">
-            <div class="btn-text img-right">
-                Elolvasom
-                <img src="<?php bloginfo('template_url') ?>/assets/img/green-long-arrow-right.png" alt="">
+<div class="col-sm-12 col-lg-4">
+    <div class="card">
+        <a href="<?php echo get_permalink(); ?>">
+            <img src="<?php echo the_post_thumbnail_url(); ?>" class="card-img-top" alt="<?php the_title(); ?>">
+            <div class="card-body">
+                <h5 class="card-title"><?php the_title(); ?></h5>
             </div>
         </a>
     </div>
