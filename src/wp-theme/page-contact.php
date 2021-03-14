ecdf1246742d14091db45bf090e7ec87666e6086
Bence Agocs <bence@harom.studio>
2021-03-14 23:23:04 +0100

    contact page added

--- src/wp-theme/page-contact.php
+++ src/wp-theme/page-contact.php
@@ -16,8 +16,8 @@ get_header();
         
         <div class="col-12 col-lg-4">
             <h3>Szigetszentmiklós</h3>
-            <img src="https://placehold.it/640x360" alt="" class="mb-3">
-            <p><i class="bi bi-geo-alt"></i> 2310 Szigetszentmiklós Gyári út 17.</p>
+            <img src="https://placehold.it/640x360" alt="">
+            <p class="mt-3"><i class="bi bi-geo-alt"></i> 2310 Szigetszentmiklós Gyári út 17.</p>
             <p><i class="bi bi-telephone"></i> +36 30 123 4567</p>
             <p><i class="bi bi-telephone"></i> +36 30 123 4567</p>
             <p><i class="bi bi-envelope"></i> info@szigetszerelveny.hu</p>
