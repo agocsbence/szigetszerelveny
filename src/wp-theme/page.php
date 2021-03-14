46a531d866f96082db8dfad474050a2dedeaaa67
Bence Agocs <bence@harom.studio>
2021-03-14 21:27:54 +0100

    page.php updated

--- src/wp-theme/page.php
+++ src/wp-theme/page.php
@@ -2,7 +2,7 @@
 
 <section class="section container">
     <h1 class="mt-5"><?php the_title() ?></h1>
-    <div class="row mt-4">
+    <div class="row bg-white p-3 mt-4">
         
         <?php the_content() ?>
 
