3fc604fcdf6e47402898928f8960c40000b6901b
Bence Agocs <bence@harom.studio>
2021-04-13 00:26:24 +0200

    js added

--- dist/wp-theme/header.php
+++ dist/wp-theme/header.php
@@ -10,6 +10,8 @@
 	<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/assets/css/all.min.css" type="text/css" />
 	<link rel="stylesheet" href="<?php bloginfo('template_url') ?>/assets/css/state.css?v=1.0.0" type="text/css" />
 
+	<script defer src="<?php bloginfo('template_url') ?>/assets/css/scripts.js"></script>
+
 	<link href="favicon.png" rel="shortcut icon" type="image/x-icon" />
 	<link href="webclip.png" rel="apple-touch-icon" />
 	<?php wp_head(); ?>
