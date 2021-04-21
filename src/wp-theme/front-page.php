1ea3a6532cc02257013eaca52b4cca1bbb462d89
Bence Agocs <bence@harom.studio>
2021-04-21 08:24:40 +0200

    home page updated

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -65,10 +65,26 @@ get_header();
 <section class="section" id="home-about">
 	<?php
 		$your_query = new WP_Query( 'pagename=rolunk' );
-		while ( $your_query->have_posts() ) : $your_query->the_post();
-			the_title();
-			the_content();
-		endwhile;
+		while ( $your_query->have_posts() ) : $your_query->the_post(); ?>
+			
+		<section class="section nolinkdecor container">
+			<h1 class="mt-5"><?php the_title() ?></h1>
+			<div class="row bg-white p-3 mt-4">
+				
+				<?php the_content() ?>
+				
+				<div class="about-icons row">
+					<div class="col"><p><i class="far fa-comment"></i> Tanácsadás</p></div>
+					<div class="col"><p><i class="fas fa-fingerprint"></i> Egyedi ajánlatok</p></div>
+					<div class="col"><p><i class="fas fa-medal"></i> Innovatív megoldások</p></div>
+					<div class="col"><p><i class="fas fa-user-check"></i> Megbízható gyártók termékei</p></div>
+					<div class="col"><p><i class="fas fa-shipping-fast"></i> Gyors teljesítés</p></div>
+				</div>
+
+			</div>
+		</section>
+
+		<?php endwhile;
 		wp_reset_postdata();
 	?>
 </section>
