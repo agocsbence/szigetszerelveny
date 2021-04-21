9c07366d1ce43c18852505f19f49e14493ba4a36
Bence Agocs <bence@harom.studio>
2021-04-21 08:27:31 +0200

    home page updated

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -89,4 +89,4 @@ get_header();
 	?>
 </section>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<?php get_footer(); ?>
