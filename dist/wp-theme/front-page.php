9c26c59d62d8e0d141d3717b0b404fbcdc2c2258
Bence Agocs <bence@harom.studio>
2021-04-21 08:21:01 +0200

    home page updated

--- dist/wp-theme/front-page.php
+++ dist/wp-theme/front-page.php
@@ -62,4 +62,15 @@ get_header();
 		</div>
 </section>
 
+<section class="section" id="home-about">
+	<?php
+		$your_query = new WP_Query( 'pagename=rolunk' );
+		while ( $your_query->have_posts() ) : $your_query->the_post();
+			the_title();
+			the_content();
+		endwhile;
+		wp_reset_postdata();
+	?>
+</section>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
