2d51112ebbbab8a457386924c6ea7349e8221dae
Bence Agocs <bence@harom.studio>
2021-04-30 13:36:39 +0200

    grid updated

--- src/wp-theme/includes/card.php
+++ src/wp-theme/includes/card.php
@@ -1,4 +1,4 @@
-<div class="col-sm-12 col-lg-4">
+<div class="col-sm-12 col-lg-3">
     <div class="card">
         <a href="<?php echo get_permalink(); ?>">
             <img src="<?php echo the_post_thumbnail_url(); ?>" class="card-img-top" alt="<?php the_title(); ?>">
