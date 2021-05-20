ccf35e61d8365067fb23dd4f69e4aa90b71ea1ad
Bence Agocs <bence@harom.studio>
2021-05-20 14:07:22 +0200

    contact page updated

--- dist/wp-theme/search.php
+++ dist/wp-theme/search.php
@@ -4,16 +4,6 @@ Template Name: Search Page
 */
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php get_search_form(); ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
-
 <section class="section nolinkdecor products search container">
 
     <div class="row mt-4">
@@ -23,19 +13,24 @@ get_header(); ?>
             <header class="page-header">
                 <h1 class="mt-5"><?php printf( __( 'Keresés: %s', 'szigetszerelveny' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
             </header>
+            <div class="mt-4">
+                <?php get_search_form(); ?>
+            </div>
 
-            <?php get_search_form(); ?>
+            <div class="mt-4">
 
-            <?php while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
 
-                <?php include get_theme_file_path( '/includes/card.php' ); ?>
+                    <?php include get_theme_file_path( '/includes/card.php' ); ?>
 
-            <?php endwhile; ?>
+                <?php endwhile; ?>
+            
+            </div>
 
 
             <?php else : ?>
 
-                <h3>A keresés sajnos nem hozott eredményt :(</h3>
+                <h3 class="mt-4">A keresés sajnos nem hozott eredményt :(</h3>
 
         <?php endif; ?>
 
