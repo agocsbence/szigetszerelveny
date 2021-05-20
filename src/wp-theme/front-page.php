fcf4bf7e0228882267d31f8fc5d03ce1c3bd485c
Bence Agocs <bence@harom.studio>
2021-05-20 15:42:53 +0200

    contact page updated

--- src/wp-theme/front-page.php
+++ src/wp-theme/front-page.php
@@ -115,7 +115,7 @@ get_header();
     <div class="row mt-4">
 
 	<?php 
-		$popularpost = new WP_Query( array( 'posts_per_page' => 3, 'meta_key' => 'wpb_post_views_count', 'orderby' => 'meta_value_num', 'order' => 'DESC'  ) );
+		$popularpost = new WP_Query( array( 'posts_per_page' => 4, 'meta_key' => 'wpb_post_views_count', 'orderby' => 'meta_value_num', 'order' => 'DESC'  ) );
 		while ( $popularpost->have_posts() ) : $popularpost->the_post(); ?>
 
 		<div class="col-sm-12 col-lg-3">
