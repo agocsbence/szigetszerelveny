9d2bb63879d6aa04bef5c9b69e28c7fc176c4f58
Bence Agocs <bence@harom.studio>
2021-06-02 13:18:05 +0200

    footer icons colorized

--- dist/wp-theme/includes/alert.php
+++ dist/wp-theme/includes/alert.php
@@ -1,3 +1,3 @@
 <section class="container mt-3">
-    <p class="alert alert-info" role="alert">Az oldalon nem a teljes kínálatot találja, látogasson el üzletünkbe vagy érdeklődjön elérhetőségeink valamelyikén raktárkészletünkről.</p>
+    <p class="alert alert-info" role="alert"><?php the_field('szoveg', 20); ?></p>
 </section>
