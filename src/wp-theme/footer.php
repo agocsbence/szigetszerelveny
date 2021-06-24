e8a61a105ffa283e5ebe1eab4c24ab5aa7d6c8cd
Bence Agocs <bence@harom.studio>
2021-06-24 23:44:42 +0200

    logo changed, data copyright adde

--- src/wp-theme/footer.php
+++ src/wp-theme/footer.php
@@ -8,12 +8,12 @@
                 <div class="col-sm-12 col-lg-4 gy-4 gy-sm-0">
                     <p><i class="text-success bi bi-telephone"></i> Szigetszentmiklós: <a href="tel:+36301234567">+36 30 123 4567</a></p>
 					<p><i class="text-success bi bi-telephone"></i> Taksony: <a href="tel:+36301234567">+36 30 123 4567</a></p>
-					<p><i class="bi bi-file-earmark-text"></i> Adatkezelési nyilatkozat</p>
+					<p><i class="bi bi-file-earmark-text"></i> <a href="<?php echo get_permalink(1507); ?>">Adatkezelés</a></p>
                 </div>
                 <div class="col-sm-12 col-lg-4 gy-4 gy-sm-0">
                     <p><i class="text-danger bi bi-geo-alt"></i> 2310 Szigetszentmiklós Gyári út 17.</p>
                     <p><i class="text-danger bi bi-geo-alt"></i> 2335 Taksony Fő út 47.</p>
-                    <p><i class="text-info bi bi-envelope"></i> info@szigetszerelveny.hu</p>
+                    <p><i class="text-info bi bi-envelope"></i> <a href="mailto:info@szigetszerelveny.hu">info@szigetszerelveny.hu</a></p>
                 </div>
             </div>
         </div>
