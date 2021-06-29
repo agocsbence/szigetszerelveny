83b09a303437ce97109948fadf76d291b79a3917
Bence Agocs <bence@harom.studio>
2021-06-29 12:32:09 +0200

    footer logo updated

--- src/wp-theme/footer.php
+++ src/wp-theme/footer.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row p-3">
                 <div class="col-sm-12 col-lg-4">
-                    <img src="<?php bloginfo('template_url') ?>/assets/img/logo-footer--white.png" alt="Sziget Szerelvény Kft." style="width: 130px;">
+                    <img src="<?php bloginfo('template_url') ?>/assets/img/logo-footer--mixed.png" alt="Sziget Szerelvény Kft." style="width: 130px;">
                 </div>
                 <div class="col-sm-12 col-lg-4 gy-4 gy-sm-0">
                     <p><i class="bi bi-telephone"></i> Szigetszentmiklós: <a href="tel:+36301234567">+36 30 123 4567</a></p>
